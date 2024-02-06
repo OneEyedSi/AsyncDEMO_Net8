475ada34e345db6d61e340f3ae2c27fd56e6ccff
SimonElms <simone@datacom.co.nz>
2024-02-06 18:25:43 +1300

    Add SimulateParallelThreads to ThreadingMenu

--- Main/ThreadingMenu.cs
+++ Main/ThreadingMenu.cs
@@ -1,6 +1,7 @@
 ﻿using AwaitDemo;
 using AwaitDemo.Tasks;
 using Common;
+using Common.MessageWriters;
 using Gold.ConsoleMenu;
 using System;
 using System.Collections.Generic;
@@ -106,6 +107,46 @@ namespace Main
             Task.Delay(taskInfo.Duration).Wait();
 
             taskInfo.WriteWithElapsedTime("Other work is finished", startTime);
+        }
+
+        [MenuMethod("Simulate parallel threads", DisplayOrder = 5)]
+        public static void SimulateParallelThreads()
+        {
+            var messageWriter = new ConsoleMessageWriter(ConsoleColor.Gray, includeThreadId: false);
+
+            messageWriter.Write();
+            messageWriter.Write("Simulating making breakfast on parallel threads:");
+            messageWriter.Write();
+
+            messageWriter.Write("Thread ID 1: Preparing breakfast:", ConsoleColor.DarkGray, indentLevel: 0);
+
+            messageWriter.Write("Thread ID 2: Heating frying pan...", ConsoleColor.Magenta, indentLevel: 1);
+            messageWriter.Write("Thread ID 3: Frying 2 eggs...", ConsoleColor.DarkRed, indentLevel: 1);
+            messageWriter.Write("Thread ID 4: Heating 3 slices of bacon...", ConsoleColor.Red, indentLevel: 1);
+            messageWriter.Write("Thread ID 5: Making coffee...", ConsoleColor.DarkYellow, indentLevel: 1);
+            messageWriter.Write("Thread ID 6: Toasting 2 slices of bread...", ConsoleColor.Yellow, indentLevel: 1);
+
+            messageWriter.Write("Thread ID 1: Caller doing other non-breakfast work...", ConsoleColor.DarkGreen, indentLevel: 0);
+
+            messageWriter.Write("Thread ID 6: 2.1 seconds elapsed: Toast has popped", ConsoleColor.Yellow, indentLevel: 1);
+            messageWriter.Write("Thread ID 2: 3.0 seconds elapsed: Frying pan is hot", ConsoleColor.Magenta, indentLevel: 1);
+            messageWriter.Write("Thread ID 3: 3.0 seconds elapsed: Eggs are ready", ConsoleColor.DarkRed, indentLevel: 1);
+            messageWriter.Write("Thread ID 5: 4.1 seconds elapsed: Coffee is ready", ConsoleColor.DarkYellow, indentLevel: 1);
+
+            messageWriter.Write("Thread ID 1: 5.1 seconds elapsed: Other work is finished", ConsoleColor.DarkGreen, indentLevel: 0);
+
+            messageWriter.Write("Thread ID 4: 5.1 seconds elapsed: Bacon is ready", ConsoleColor.Red, indentLevel: 1);
+            messageWriter.Write("Thread ID 6: Spreading butter on 2 slices of toast...", ConsoleColor.Cyan, indentLevel: 1);
+            messageWriter.Write("Thread ID 6: 6.1 seconds elapsed: Butter has been spread", ConsoleColor.Cyan, indentLevel: 1);
+            messageWriter.Write("Thread ID 6: Spreading jam on 2 slices of toast...", ConsoleColor.DarkCyan, indentLevel: 1);
+            messageWriter.Write("Thread ID 6: 7.1 seconds elapsed: Jam has been spread", ConsoleColor.DarkCyan, indentLevel: 1);
+            messageWriter.Write("Thread ID 1: Pouring orange juice...", ConsoleColor.Blue, indentLevel: 1);
+            messageWriter.Write("Thread ID 1: 7.6 seconds elapsed: Juice is ready", ConsoleColor.Blue, indentLevel: 1);
+
+            messageWriter.Write("Thread ID 1: 7.6 seconds elapsed: Breakfast is ready!", ConsoleColor.DarkGray, indentLevel: 0);
+
+            messageWriter.Write();
+            messageWriter.Write("TOTAL TIME TAKEN: 7.6 seconds", ConsoleColor.Gray, indentLevel: 0);
         }
     }
 }
